73c0cbe0fbc4a0b4eac7fda2d779cb830a03e3ec
RohithR24 <srohithreddy.edu@gmail.com>
2024-06-26 19:48:06 -0700

    Adding next nav (URL params)

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,28 +1,93 @@
-"use client"
-import React from 'react'
-import {useState} from 'react';
+"use client";
+import React from "react";
+import { useState } from "react";
 
-import {SearchManufacturer} from '@/components';
-import { manufacturers } from '@/constants';
+import { SearchManufacturer } from "@/components";
+import { manufacturers } from "@/constants";
+import Image from "next/image";
+import {useRouter} from 'next/navigation';
+
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => {
+  return (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image
+        src={"/magnifying-glass.svg"}
+        alt={"magnifying glass"}
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </button>
+  );
+};
 const SearchBar = () => {
- 
-    const [manufacturer, setManufacturer] = useState('');
-    const handleSearch = () => {
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar");
+    }
+
+    updateSearchParams( model.toLowerCase(),manufacturer.toLowerCase())
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
 
 
-  }
+    if (model) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+
+    router.push(newPathName)
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-        
-        <div className="searchbar__item">
-            <SearchManufacturer
-                manufacturer = {manufacturer}
-                setManufacturer = {setManufacturer}
-            />
-        </div>
-    </form>
-  )
-}
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
 
-export default SearchBar
+        <SearchButton otherClasses={"sm:hidden"} />
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Model"
+          className="searchbar__input"
+        />
+
+        <SearchButton otherClasses={"sm:hidden"} />
+        <SearchButton otherClasses={"max-sm:hidden"} />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
