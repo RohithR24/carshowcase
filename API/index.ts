b8e513d255b31b053323e468f11cd8601b626591
RohithR24 <srohithreddy.edu@gmail.com>
2024-06-27 19:52:46 -0700

    Adding URL params dynamically

--- API/index.ts
+++ API/index.ts
@@ -1,32 +1,36 @@
-
-import { FilterProps} from '@/types'; 
-
+import { FilterProps } from "@/types";
 
 // Returns the response from the API
 export async function fetchCars(filters: FilterProps) {
-  const {model, year, fuel, manufacturer} = filters;
-    const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=${model}&make=${manufacturer}`;
+  const { model, year, fuel, manufacturer } = filters;
 
-    console.log('URL', url);
-    //url.searchParams.append('model', )
-    
-    const apiKey = process.env.X_RAPIDAPI_KEY || '';
-    const apiHost = process.env.X_RAPIDAPI_HOST || '';
-  
-    const options = {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': apiKey,
-        'x-rapidapi-host': apiHost
-      }
-    };
-  
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.error(error);
-    }
+  const Url = new URL("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars");
+  Url.searchParams.append("model", model);
+  Url.searchParams.append("make", manufacturer);
+  Url.searchParams.append("year", `${year}`);
+  Url.searchParams.append("fuel", fuel);
+
+  // const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=${model}&make=${manufacturer}`;
+
+  // console.log("URL 1", url);
+  // console.log("URL 2", Url);
+
+  const apiKey = process.env.X_RAPIDAPI_KEY || "";
+  const apiHost = process.env.X_RAPIDAPI_HOST || "";
+
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": apiKey,
+      "x-rapidapi-host": apiHost,
+    },
+  };
+
+  try {
+    const response = await fetch(Url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
   }
-  
+}
